021afa4c1fb170d6a437d3621be7bd7a5a11e0f4
shoaib <Shoaib.Hasan1801@gmail.com>
2022-06-10 04:26:59 -0700

    Initial commit

---
+++ src/main/java/com/shoaib/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package com.shoaib.coronavirustracker.controllers;
+
+import com.shoaib.coronavirustracker.models.LocationStats;
+import com.shoaib.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String Home(Model model) throws IOException, InterruptedException {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPreviousDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        model.addAttribute("localDateTime", LocalDateTime.now());
+
+        return "home";
+    }
+}
